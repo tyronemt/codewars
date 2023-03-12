f73f0e729f84fc0f586b12b96ae9adcd11c277b3
Tyrone Tong <tyrone.tong800@gmail.com>
2023-03-11 16:00:42 -0800

    completed codewars challenge

---
+++ top_3_words.py
@@ -0,0 +1,6 @@
+import re
+from collections import Counter
+def top_3_words(text):
+    c = Counter(re.findall(r"[a-z']+", re.sub(r" '+ ", " ", text.lower())))
+    
+    return [w for w,_ in c.most_common(3)]
